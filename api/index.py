5feac958a12bd1bdf86fc1ce0563f5ec68c6dc2d
drec0xy <timmydchoune@gmail.com>
2023-05-02 00:04:47 +0100

    added infere endpoint

--- api/index.py
+++ api/index.py
@@ -10,6 +10,6 @@ def home():
 def about():
     return 'About'
 
-@app.route('/infere?<inference>', ['GET'])
+@app.route('/infere/<inference>', ['GET'])
 def infer(inference):
     return jsonify(userQuery = "can you do something for me?", botResponds = "Sure what is that?",queryTimestamp = "2013-10-21T13:28:06.419Z",respondTimestamp = "2013-10-21T13:28:06.419Z")
