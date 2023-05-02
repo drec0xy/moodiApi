021d9fef8b1c16cd005baa7517146c1622c5091b
drec0xy <timmydchoune@gmail.com>
2023-05-02 11:32:10 +0100

    infere-working👌🚀

--- api/index.py
+++ api/index.py
@@ -1,4 +1,5 @@
 from flask import Flask,jsonify
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -10,6 +11,10 @@ def home():
 def about():
     return 'About'
 
-@app.route('/infere/<inference>', ['GET'])
+@app.route('/infere/<inference>')
 def infer(inference):
-    return jsonify(userQuery = "can you do something for me?", botResponds = "Sure what is that?",queryTimestamp = "2013-10-21T13:28:06.419Z",respondTimestamp = "2013-10-21T13:28:06.419Z")
+    return jsonify(userQuery = inference, botResponds = "Sure what is that?",queryTimestamp = "2013-10-21T13:28:06.419Z",respondTimestamp = datetime.now())
+
+
+if __name__ == '__main__':
+    app.run()
