5a17cb681b1d7d72b07c7f09da6c7dcd5355a5d8
drec0xy <timmydchoune@gmail.com>
2023-05-02 11:43:53 +0100

    infere-working👌🚀

--- api/index.py
+++ api/index.py
@@ -13,7 +13,7 @@ def about():
 
 @app.route('/infere/<inference>')
 def infer(inference):
-    return jsonify(userQuery = inference, botResponds = "Sure what is that?",queryTimestamp = "2013-10-21T13:28:06.419Z",respondTimestamp = datetime.now())
+    return jsonify(userQuery = inference, botResponds = "please comeback soon, Moodichatbot is still under developement.",queryTimestamp = "2013-10-21T13:28:06.419Z",respondTimestamp = datetime.now())
 
 
 if __name__ == '__main__':
