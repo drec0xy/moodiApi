b521699edc4e096f500599db53ca33cf843329bc
drec0xy <timmydchoune@gmail.com>
2023-05-30 15:04:06 +0100

    bot working🎊

--- server/src/inferer.py
+++ server/src/inferer.py
@@ -28,7 +28,7 @@ from transformers import BartTokenizer, BartForConditionalGeneration
 
 # load bart tokenizer and model from huggingface
 tokenizer = BartTokenizer.from_pretrained('vblagoje/bart_lfqa')
-generator = BartForConditionalGeneration.from_pretrained('vblagoje/bart_lfqa').to("gpu")
+generator = BartForConditionalGeneration.from_pretrained('vblagoje/bart_lfqa').to(device)
 
 def query_pinecone(query, top_k):
     # generate embeddings for the query
